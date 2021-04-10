d02e528824fb424d45069ee2f2816ded3089a4c4
S Mautner <mautner@cs.uni-freiburg.de>
2021-04-10 23:29:45 +0200

    board decoration and switching sides for black

--- bunseki/makequiz.py
+++ bunseki/makequiz.py
@@ -3,11 +3,37 @@ import pprint  as p
 from bunseki import util
 import basics as ba
 
-def decorate_unicode(board): 
-    print(b)
-    print()
-    for e in b.split('\n'):
-        print(list(e))
+
+
+def putthis(bline,sq,symbol):
+
+    sq_b ="▢"
+    sq_b= '□'
+    sq_w ='■' 
+
+    #sq_b=' '
+
+    if symbol != 'x':
+        return symbol
+    else:
+        ret =  sq_b
+        if ((bline%2) + (sq%4 == 0))%2:
+            ret = sq_w 
+    return ret
+
+
+def decorate_unicode(b,color): 
+
+    boardlines = [] 
+    for i,line  in enumerate(b.split('\n')):
+        newline = [ putthis(i,e,symbol)  for e, symbol in enumerate(line)  ]
+        boardlines.append(''.join(newline))
+
+    if color == 'black':
+        boardlines.reverse()
+        boardlines = [ s[::-1] for s in boardlines]
+    return "\n".join(boardlines)
+        
 
 def add_to_data(gn, color): 
     # is it our turn? 
@@ -28,7 +54,8 @@ def makequiz(color,inpgn, outdeck):
         n = nodes.pop()
         gn,mv = add_to_data(n, color)
         if gn:
-            b = gn.board().unicode(invert_color=True)
+            b = gn.board().unicode(invert_color=True, empty_square='x')
+            b = decorate_unicode(b,color= color)
             data[hash(gn.board().fen())] = {'q':b, 'a':mv, 'sort':sort}
             sort+=1
 
